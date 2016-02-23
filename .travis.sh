0a746cae83a4007c62fe50ae97de8acc523e76c0
maek <maek@paranoici.org>
2016-02-23 21:58:48 +0000

    Update Travis CI scripts

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+# Copyright (C) 2015-2016  Unreal Arena
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Travis CI support script
 
+
+################################################################################
+# Setup
+################################################################################
 
 # Arguments parsing
 if [ $# -ne 1 ]; then
@@ -10,21 +29,29 @@ if [ $# -ne 1 ]; then
 fi
 
 
-# Routines ---------------------------------------------------------------------
-
-# linux
+################################################################################
+# Routines (linux)
+################################################################################
 
 # before_install
 linux-before_install() {
-	sudo add-apt-repository -y ppa:smspillaz/cmake-2.8.12
+	sudo add-apt-repository -y ppa:nschloe/cmake-backports
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get -qq update
 }
 
 # install
 linux-install() {
-	sudo apt-get -qq install cmake cmake-data gcc-4.7 g++-4.7 libgl1-mesa-dev libx11-dev libxext-dev zip
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 100 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7
+	sudo apt-get -qq install cmake\
+	                         cmake-data\
+	                         gcc-4.7\
+	                         g++-4.7\
+	                         libgl1-mesa-dev\
+	                         libx11-dev\
+	                         libxext-dev\
+	                         zip
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 100\
+	                         --slave   /usr/bin/g++ g++ /usr/bin/g++-4.7
 }
 
 # before_script
@@ -34,36 +61,39 @@ linux-before_script() {
 
 # script
 linux-script() {
-	mkdir build
-	cd build
-	cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release\
-	                          -DBUILD_GAME_NATIVE_EXE=0\
-	                          -DBUILD_GAME_NATIVE_DLL=0\
-	                          ..
-	cmake --build . -- -j8 || cmake --build . -- VERBOSE=1
+	cmake -H. -Bbuild -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release\
+	                                      -DBUILD_GAME_NATIVE_EXE=0\
+	                                      -DBUILD_GAME_NATIVE_DLL=0
+	cmake --build build -- -j8 || cmake --build build -- VERBOSE=1
 }
 
 # before_deploy
 linux-before_deploy() {
-	cd build
-	wget -q "http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz" && gunzip "GeoIP.dat.gz"
-	wget -q "http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz" && gunzip "GeoIPv6.dat.gz"
-	zip -9 "../unrealarena-linux.pre.zip" daemon\
-	                                      daemon-tty\
-	                                      daemonded\
-	                                      irt_core-x86_64.nexe\
-	                                      nacl_helper_bootstrap\
-	                                      nacl_loader\
-	                                      cgame-x86-stripped.nexe\
-	                                      sgame-x86-stripped.nexe\
-	                                      cgame-x86_64-stripped.nexe\
-	                                      sgame-x86_64-stripped.nexe\
-	                                      GeoIP.dat\
-	                                      GeoIPv6.dat
+	curl -Ls "http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz" |
+		gunzip > build/GeoIP.dat
+	curl -Ls "http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz" |
+		gunzip > build/GeoIPv6.dat
+	cd build && zip -r9 --symlinks "../unrealarena-${TRAVIS_OS_NAME}.pre.zip" daemon\
+	                                                                          daemonded\
+	                                                                          daemon-tty\
+	                                                                          irt_core-x86_64.nexe\
+	                                                                          libGLEW.so.1.12\
+	                                                                          libopenal.so.1\
+	                                                                          libSDL2-2.0.so.0\
+	                                                                          nacl_helper_bootstrap\
+	                                                                          nacl_loader\
+	                                                                          cgame-x86-stripped.nexe\
+	                                                                          sgame-x86-stripped.nexe\
+	                                                                          cgame-x86_64-stripped.nexe\
+	                                                                          sgame-x86_64-stripped.nexe\
+	                                                                          GeoIP.dat\
+	                                                                          GeoIPv6.dat
 }
 
 
-# Main -------------------------------------------------------------------------
+################################################################################
+# Main
+################################################################################
 
 # Arguments check
 if ! `declare -f "${TRAVIS_OS_NAME}-${1}" > /dev/null`; then
