c53a34288d8b36249565245da1aec9856c6d4a0a
maek <maek@paranoici.org>
2018-07-15 14:54:25 +0100

    Fix NaCl crash handling w/o breakpad

--- daemon/src/common/System.cpp
+++ daemon/src/common/System.cpp
@@ -1,32 +1,30 @@
 /*
-===========================================================================
-Daemon BSD Source Code
-Copyright (c) 2013-2014, Daemon Developers
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Daemon developers nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL DAEMON DEVELOPERS BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-===========================================================================
-*/
+ * Copyright (c) 2018, Unreal Arena
+ * Copyright (c) 2013-2014, Daemon Developers
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "Common.h"
 #ifdef _WIN32
@@ -37,7 +35,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <signal.h>
 #ifdef __native_client__
 #include <nacl/nacl_exception.h>
+#ifdef UNREALARENA
+#ifdef USE_BREAKPAD
 #include <nacl/nacl_minidump.h>
+#endif
+#else
+#include <nacl/nacl_minidump.h>
+#endif
 #include <nacl/nacl_random.h>
 #include "engine/client/cg_api.h"
 #else
@@ -235,16 +239,39 @@ void SetupCrashHandler()
 	SetUnhandledExceptionFilter(CrashHandler);
 }
 #elif defined(__native_client__)
+#ifdef UNREALARENA
+#ifdef USE_BREAKPAD
 static void CrashHandler(const void* data, size_t n)
+#else
+static void CrashHandler(struct NaClExceptionContext *)
+#endif
+#else
+static void CrashHandler(const void* data, size_t n)
+#endif
 {
+#ifdef UNREALARENA
+#ifdef USE_BREAKPAD
     trap_CrashDump(static_cast<const uint8_t*>(data), n);
+#endif
+#else
+    trap_CrashDump(static_cast<const uint8_t*>(data), n);
+#endif
     Sys::Error("Crashed with NaCl exception");
 }
 
 void SetupCrashHandler()
 {
+#ifdef UNREALARENA
+#ifdef USE_BREAKPAD
     nacl_minidump_register_crash_handler();
     nacl_minidump_set_callback(CrashHandler);
+#else
+	nacl_exception_set_handler(CrashHandler);
+#endif
+#else
+    nacl_minidump_register_crash_handler();
+    nacl_minidump_set_callback(CrashHandler);
+#endif
 }
 #else
 static void CrashHandler(int sig)
